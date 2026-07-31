ce7ccd419518cb5893a7a9d00002817aa109c2a0
Jasim1989 <your-email@example.com>
2026-07-31 23:59:06 +0000

    تجهيز الكود للنشر السحابي

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const http = require('http');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-    res.end(JSON.stringify({
-        message: "مرحباً بك في تطبيق Aegis!",
-        status: "شغال بنجاح وبدون مشاكل 🚀"
-    }));
+  res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+  res.end(JSON.stringify({
+    message: "مرحباً بك في تطبيق Aegis!",
+    status: "شغال وبنجاح! 🚀"
+  }));
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
